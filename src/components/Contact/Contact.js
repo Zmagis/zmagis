94d761956c4baf8560ad29522fa338fe9f1f8aeb
Egle Gustaite <eglegustaite@gmail.com>
2019-11-27 18:06:23 +0200

    fix contact with netlify

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Contact.css";
 
-class Contact extends React.Component {
+const Contact = () => {
   // constructor(props) {
   //   super(props);
   //   this.state = {
@@ -12,62 +12,62 @@ class Contact extends React.Component {
   //     phone: ""
   //   };
   // }
-  render() {
-    return (
-      <div className="screen">
-        <div className="title">
-          <h1>Contact</h1>
-        </div>
-        <form name="contact" netlify>
-          <input
-            className="first-name"
-            type="text"
-            placeholder="First Name"
-            // value={this.state.firstName}
-            // onChange={e => this.setState({ firstName: e.target.value })}
-            name="first name"
-          />
-          <input
-            className="last-name"
-            type="text"
-            placeholder="Last Name"
-            // value={this.state.LastName}
-            // onChange={e => this.setState({ lastName: e.target.value })}
-            name="last name"
-          />
-          <input
-            className="email"
-            type="email"
-            required
-            placeholder="Email"
-            // value={this.state.email}
-            // onChange={e => this.setState({ email: e.target.value })}
-            name="email"
-          />
-          <input
-            className="phone"
-            placeholder="Phone"
-            // value={this.state.phone}
-            // onChange={e => this.setState({ phone: e.target.value })}
-            type="phone"
-            name="phone"
-          />
-          <textarea
-            className="message"
-            type="text"
-            required
-            placeholder="Message"
-            // value={this.state.message}
-            // onChange={e => this.setState({ message: e.target.value })}
-            name="message"
-          />
-          <button className="submit" type="submit" value="Send" name="button">
-            Send
-          </button>
-        </form>
+  // render() {
+  return (
+    <div className="screen">
+      <div className="title">
+        <h1>Contact</h1>
       </div>
-    );
-  }
-}
+      <form name="contact" netlify>
+        <input
+          className="first-name"
+          type="text"
+          placeholder="First Name"
+          // value={this.state.firstName}
+          // onChange={e => this.setState({ firstName: e.target.value })}
+          name="first-name"
+        />
+        <input
+          className="last-name"
+          type="text"
+          placeholder="Last Name"
+          // value={this.state.LastName}
+          // onChange={e => this.setState({ lastName: e.target.value })}
+          name="last-name"
+        />
+        <input
+          className="email"
+          type="email"
+          required
+          placeholder="Email"
+          // value={this.state.email}
+          // onChange={e => this.setState({ email: e.target.value })}
+          name="email"
+        />
+        <input
+          className="phone"
+          placeholder="Phone"
+          // value={this.state.phone}
+          // onChange={e => this.setState({ phone: e.target.value })}
+          type="phone"
+          name="phone"
+        />
+        <textarea
+          className="message"
+          type="text"
+          required
+          placeholder="Message"
+          // value={this.state.message}
+          // onChange={e => this.setState({ message: e.target.value })}
+          name="message"
+        />
+        <button className="submit" type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+// }
 
 export default Contact;
